just-a-pony/avm

commit b727526f33a1b84f39afaeff7cc66a2cbf803cde

Remove redundant ref_mv_bank_pt pointer
Change-Id: I75db50dba49e5ff90c51f99d84d49eff2ed813d5

av1/common/blockd.h
@@ -852,8 +852,7 @@ typedef struct macroblockd {
    * \name Reference MV bank info.
    */
   /**@{*/
-  REF_MV_BANK ref_mv_bank;     /*!< Ref mv bank to update */
-  REF_MV_BANK *ref_mv_bank_pt; /*!< Pointer to bank to refer to */
+  REF_MV_BANK ref_mv_bank; /*!< Ref mv bank to update */
   /**@}*/
 #endif  // CONFIG_REF_MV_BANK
 

av1/common/mvref_common.c
@@ -1731,7 +1731,7 @@ static AOM_INLINE void setup_ref_mv_list(
       && ref_frame != INTRA_FRAME
 #endif  // CONFIG_BVP_IMPROVEMENT
   ) {
-    const REF_MV_BANK *ref_mv_bank = xd->ref_mv_bank_pt;
+    const REF_MV_BANK *ref_mv_bank = &xd->ref_mv_bank;
     const CANDIDATE_MV *queue = ref_mv_bank->rmb_buffer[ref_frame];
     const int count = ref_mv_bank->rmb_count[ref_frame];
     const int start_idx = ref_mv_bank->rmb_start_idx[ref_frame];

av1/decoder/decodeframe.c
@@ -3495,7 +3495,6 @@ static AOM_INLINE void decode_tile(AV1Decoder *pbi, ThreadData *const td,
     av1_zero_left_context(xd);
 #if CONFIG_REF_MV_BANK
     av1_zero(xd->ref_mv_bank);
-    xd->ref_mv_bank_pt = &td->ref_mv_bank;
 #endif  // CONFIG_REF_MV_BANK
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;
@@ -3994,7 +3993,6 @@ static AOM_INLINE void parse_tile_row_mt(AV1Decoder *pbi, ThreadData *const td,
     av1_zero_left_context(xd);
 #if CONFIG_REF_MV_BANK
     av1_zero(xd->ref_mv_bank);
-    xd->ref_mv_bank_pt = &td->ref_mv_bank;
 #endif  // CONFIG_REF_MV_BANK
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;

av1/encoder/encodeframe.c
@@ -936,7 +936,6 @@ void av1_encode_tile(AV1_COMP *cpi, ThreadData *td, int tile_row,
        mi_row += cm->seq_params.mib_size) {
 #if CONFIG_REF_MV_BANK
     av1_zero(td->mb.e_mbd.ref_mv_bank);
-    td->mb.e_mbd.ref_mv_bank_pt = &td->mb.e_mbd.ref_mv_bank;
 #endif  // CONFIG_REF_MV_BANK
     av1_encode_sb_row(cpi, td, tile_row, tile_col, mi_row);
   }

av1/encoder/ethread.c
@@ -475,7 +475,6 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
         &td->mb.txfm_search_info.mb_rd_record.crc_calculator);
 #if CONFIG_REF_MV_BANK
     av1_zero(td->mb.e_mbd.ref_mv_bank);
-    td->mb.e_mbd.ref_mv_bank_pt = &td->mb.e_mbd.ref_mv_bank;
 #endif  // CONFIG_REF_MV_BANK}
 
     av1_encode_sb_row(cpi, td, tile_row, tile_col, current_mi_row);
@@ -800,8 +799,6 @@ static AOM_INLINE void prepare_enc_workers(AV1_COMP *cpi, AVxWorkerHook hook,
     }
 #if CONFIG_REF_MV_BANK
     av1_zero(thread_data->td->mb.e_mbd.ref_mv_bank);
-    thread_data->td->mb.e_mbd.ref_mv_bank_pt =
-        &thread_data->td->mb.e_mbd.ref_mv_bank;
 #endif  // CONFIG_REF_MV_BANK
   }
 }

commit 1a1775fcd3ba1dd6ac11e868166e82c021b69e53

fix mismatch when C043 macro off
fix issue #101, after study, this issue is cause by remove ref_mv_bank_pt pointer introduced by b727526f
this fix revoke the changes under C043 macro

av1/common/blockd.h
@@ -914,6 +914,9 @@ typedef struct macroblockd {
    * \name Reference MV bank info.
    */
   /**@{*/
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+  REF_MV_BANK *ref_mv_bank_pt; /*!< Pointer to bank to refer to */
+#endif
   REF_MV_BANK ref_mv_bank; /*!< Ref mv bank to update */
   /**@}*/
 #endif  // CONFIG_REF_MV_BANK

av1/common/mvref_common.c
@@ -2060,7 +2060,7 @@ static AOM_INLINE void setup_ref_mv_list(
       && ref_frame != INTRA_FRAME
 #endif  // CONFIG_BVP_IMPROVEMENT
   ) {
-    const REF_MV_BANK *ref_mv_bank = &xd->ref_mv_bank;
+    const REF_MV_BANK *ref_mv_bank = xd->ref_mv_bank_pt;
     const CANDIDATE_MV *queue = ref_mv_bank->rmb_buffer[ref_frame];
     const int count = ref_mv_bank->rmb_count[ref_frame];
     const int start_idx = ref_mv_bank->rmb_start_idx[ref_frame];

av1/decoder/decodeframe.c
@@ -3477,6 +3477,9 @@ static AOM_INLINE void decode_tile(AV1Decoder *pbi, ThreadData *const td,
     av1_zero_left_context(xd);
 #if CONFIG_REF_MV_BANK
     av1_zero(xd->ref_mv_bank);
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+    xd->ref_mv_bank_pt = &td->ref_mv_bank;
+#endif
 #endif  // CONFIG_REF_MV_BANK
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;
@@ -3975,6 +3978,9 @@ static AOM_INLINE void parse_tile_row_mt(AV1Decoder *pbi, ThreadData *const td,
     av1_zero_left_context(xd);
 #if CONFIG_REF_MV_BANK
     av1_zero(xd->ref_mv_bank);
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+    xd->ref_mv_bank_pt = &td->ref_mv_bank;
+#endif
 #endif  // CONFIG_REF_MV_BANK
 
     for (int mi_col = tile_info.mi_col_start; mi_col < tile_info.mi_col_end;

av1/encoder/encodeframe.c
@@ -935,6 +935,9 @@ void av1_encode_tile(AV1_COMP *cpi, ThreadData *td, int tile_row,
        mi_row += cm->seq_params.mib_size) {
 #if CONFIG_REF_MV_BANK
     av1_zero(td->mb.e_mbd.ref_mv_bank);
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+    td->mb.e_mbd.ref_mv_bank_pt = &td->mb.e_mbd.ref_mv_bank;
+#endif
 #endif  // CONFIG_REF_MV_BANK
     av1_encode_sb_row(cpi, td, tile_row, tile_col, mi_row);
   }

av1/encoder/ethread.c
@@ -475,6 +475,9 @@ static int enc_row_mt_worker_hook(void *arg1, void *unused) {
         &td->mb.txfm_search_info.mb_rd_record.crc_calculator);
 #if CONFIG_REF_MV_BANK
     av1_zero(td->mb.e_mbd.ref_mv_bank);
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+    td->mb.e_mbd.ref_mv_bank_pt = &td->mb.e_mbd.ref_mv_bank;
+#endif
 #endif  // CONFIG_REF_MV_BANK}
 
     av1_encode_sb_row(cpi, td, tile_row, tile_col, current_mi_row);
@@ -799,6 +802,11 @@ static AOM_INLINE void prepare_enc_workers(AV1_COMP *cpi, AVxWorkerHook hook,
     }
 #if CONFIG_REF_MV_BANK
     av1_zero(thread_data->td->mb.e_mbd.ref_mv_bank);
+#if !CONFIG_C043_MVP_IMPROVEMENTS
+    thread_data->td->mb.e_mbd.ref_mv_bank_pt =
+        &thread_data->td->mb.e_mbd.ref_mv_bank;
+
+#endif
 #endif  // CONFIG_REF_MV_BANK
   }
 }