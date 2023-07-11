8801c415828f53cad0ab34a65d9216080df36125
Hilmi Egilmez <hegilmez@apple.com>
2023-07-11 18:44:52 +0000

    Fix a bug in CONFIG_ATC_DCTX_ALIGNED(issue #187)
    * The fix is a lossless change under CTC.
    * The change only has an impact when reduced-tx-type-set is enabled. It reverts back to the intended design.
    * The fixed design based on adopted CWG-C073 provides -1.27% AI YPSNR BD-rate and about -0.5% RA YPSNR BD-rate gains over the AVM-46c9dd512 (Overall Mandatory w/o B2) when reduced-tx-type-set is enabled in both anchor and test.

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -2456,11 +2456,7 @@ static INLINE TxSetType av1_get_ext_tx_set_type(TX_SIZE tx_size, int is_inter,
     return is_inter ? EXT_TX_SET_DCT_IDTX : EXT_TX_SET_DCTONLY;
 #endif  // CONFIG_ATC_DCTX_ALIGNED
 #if CONFIG_ATC_REDUCED_TXSET
-#if CONFIG_ATC_DCTX_ALIGNED
-  if (use_reduced_set) return EXT_TX_SET_DCT_IDTX;
-#else
   if (use_reduced_set) return is_inter ? EXT_TX_SET_DCT_IDTX : EXT_NEW_TX_SET;
-#endif  // CONFIG_ATC_DCTX_ALIGNED
 #else
   if (use_reduced_set)
     return is_inter ? EXT_TX_SET_DCT_IDTX : EXT_TX_SET_DTT4_IDTX;
