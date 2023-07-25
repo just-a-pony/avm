55c6df3a0b8852e681d82a6d3a365c723a2edc47
Byeongdoo Choi <bdchoi@amazon.com>
2023-07-25 02:49:14 +0000

    Fix on decrease_ref_count for forward key frame
    To avoid a duplicate decrement of ref count with show_existing_frame == 1 for forward key frame.
    It can address #209.

--- av1/av1_dx_iface.c
+++ av1/av1_dx_iface.c
@@ -617,7 +617,8 @@ static aom_codec_err_t decoder_decode(aom_codec_alg_priv_t *ctx,
     // show_existing_frame
     if (pbi->common.seq_params.order_hint_info.enable_order_hint &&
         pbi->common.seq_params.enable_frame_output_order) {
-      decrease_ref_count(pbi->output_frames[0], pool);
+      if (!pbi->common.show_existing_frame)
+        decrease_ref_count(pbi->output_frames[0], pool);
     } else {
 #endif  // CONFIG_OUTPUT_FRAME_BASED_ON_ORDER_HINT
       for (size_t j = 0; j < pbi->num_output_frames; j++) {
