8f9f2e4d1a04e721b8f45d35bbee727ab07b05fb
Wan-Teh Chang <wtc@google.com>
2024-03-21 13:39:06 -0700

    Clarify the help message for CONFIG_DEBUG
    This is a port of the libaom CL
    https://aomedia-review.googlesource.com/c/aom/+/122803.
    
    Change the help message for CONFIG_DEBUG from "Debug build flag." to
    "Enable debug-only code." The original help message could be
    misinterpreted as enabling CMAKE_BUILD_TYPE=Debug.

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -84,7 +84,7 @@ set_aom_config_var(CONFIG_SHARED 0 "Build shared libs.")
 set_aom_config_var(CONFIG_WEBM_IO 1 "Enables WebM support.")
 
 # Debugging flags.
-set_aom_config_var(CONFIG_DEBUG 0 "Debug build flag.")
+set_aom_config_var(CONFIG_DEBUG 0 "Enable debug-only code.")
 set_aom_config_var(CONFIG_MISMATCH_DEBUG 0 "Mismatch debugging flag.")
 set_aom_config_var(CONFIG_EXCLUDE_SIMD_MISMATCH 1
                    "Exclude mismatch in SIMD functions for testing/debugging.")
