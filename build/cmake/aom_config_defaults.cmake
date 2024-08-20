b08a2d638b28ee5fcae4a4ff54a2719785799490
Leo (Liang) Zhao <leolzhao@global.tencent.com>
2024-08-20 03:58:35 +0000

    Disable CONFIG_E125_MHCCP_SIMPLIFY
    There is a CI/CD unit test failure #443  due to this compile time flag. So turning it off to not block the merging of other MRs.

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -369,7 +369,7 @@ set_aom_config_var(CONFIG_BLEND_MODE 1
                    "Enable improved intra blend mode from CWG-D046")
 set_aom_config_var(CONFIG_ENABLE_MHCCP 1
                    "Enable multi hypothesis cross component prediction")
-set_aom_config_var(CONFIG_E125_MHCCP_SIMPLIFY 1
+set_aom_config_var(CONFIG_E125_MHCCP_SIMPLIFY 0
                    "Simplify the parameter derivation for MHCCP")
 set_aom_config_var(CONFIG_C071_SUBBLK_WARPMV 1
                    "AV2 experiment flag to use subblock warp MV for SMVP")
