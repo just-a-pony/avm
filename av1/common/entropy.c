d6d6a42a5988d550a364c74b076877610abb3641
Lin Zheng <linzhen@google.com>
2024-12-10 21:39:03 +0000

    Remove redundant inter_ext_tx_cdf counter reset
    Fix issue #541
    
    STATS_CHANGED

--- av1/common/entropy.c
+++ av1/common/entropy.c
@@ -474,9 +474,6 @@ void av1_reset_cdf_symbol_counters(FRAME_CONTEXT *fc) {
                            CDF_SIZE(TX_TYPES));
   RESET_CDF_COUNTER_STRIDE(fc->inter_ext_tx_cdf[3], INTER_TX_SET3,
                            CDF_SIZE(TX_TYPES));
-  RESET_CDF_COUNTER_STRIDE(fc->inter_ext_tx_cdf[1], 16, CDF_SIZE(TX_TYPES));
-  RESET_CDF_COUNTER_STRIDE(fc->inter_ext_tx_cdf[2], 12, CDF_SIZE(TX_TYPES));
-  RESET_CDF_COUNTER_STRIDE(fc->inter_ext_tx_cdf[3], 2, CDF_SIZE(TX_TYPES));
 #if CONFIG_TX_TYPE_FLEX_IMPROVE
   RESET_CDF_COUNTER(fc->inter_ext_tx_short_side_cdf, 4);
   RESET_CDF_COUNTER(fc->intra_ext_tx_short_side_cdf, 4);
