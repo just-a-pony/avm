49bf90b263078feb17224b5300df2d996f2967ef
Wan-Teh Chang <wtc@google.com>
2025-05-28 14:17:22 -0700

    Remove CONFIG_QM_SIMPLIFY of the CWG-D146 proposal
    The QM generation code under CONFIG_QM_SIMPLIFY was added in
    research-v8.1.0:
    https://gitlab.com/AOMediaCodec/avm/-/commit/f6ff070980

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -546,8 +546,6 @@ set_aom_config_var(CONFIG_LF_SUB_PU 1 "AV2 enable LF on sub blocks")
 set_aom_config_var(CONFIG_ASYM_DF 1 "Enable asymmetric DF")
 set_aom_config_var(CONFIG_DERIVED_MVD_SIGN 1 "Enable MVD sign derivations")
 set_aom_config_var(CONFIG_VQ_MVD_CODING 1 "Enable VQ based MVD coding")
-set_aom_config_var(CONFIG_QM_SIMPLIFY 1
-                   "Enable new QMs based on 8x8, 8x4, and 4x8 sizes.")
 set_aom_config_var(CONFIG_OPFL_BI 1 "Enable bilinear for initial opfl.")
 set_aom_config_var(CONFIG_SUBBLK_REF_EXT 1
                    "Enable extension for subblock MV refinement.")
