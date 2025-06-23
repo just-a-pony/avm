7f84832aa15b4b1dd885546495bfbd85523e7a0a
Wan-Teh Chang <wtc@google.com>
2025-06-23 17:55:45 +0000

    Add reproducer test for issue 717 as disabled test
    For #717

--- test/encode_api_test.cc
+++ test/encode_api_test.cc
@@ -94,6 +94,164 @@ TEST(EncodeAPI, EncodeOddWidthHeight420) {
   EXPECT_EQ(aom_codec_destroy(&enc), AOM_CODEC_OK);
 }
 
+// A test that reproduces https://gitlab.com/AOMediaCodec/avm/-/issues/717.
+TEST(EncodeAPI, DISABLED_AvmIssue717Width2) {
+  constexpr unsigned int kWidth = 2;
+  constexpr unsigned int kHeight = 34;
+  constexpr unsigned int kChromaWidth = kWidth / 2;
+  constexpr unsigned int kChromaHeight = kHeight / 2;
+  uint16_t buf[kWidth * kHeight + 2 * kChromaWidth * kChromaHeight] = {
+    // Y plane:
+    0, 30, 30, 60, 60, 90, 90, 120,          //
+    120, 150, 150, 180, 180, 210, 210, 240,  //
+    240, 270, 270, 300, 300, 330, 330, 361,  //
+    361, 391, 391, 421, 421, 451, 451, 481,  //
+    481, 511, 511, 541, 541, 571, 571, 601,  //
+    601, 631, 631, 661, 661, 692, 692, 722,  //
+    722, 752, 752, 782, 782, 812, 812, 842,  //
+    842, 872, 872, 902, 902, 932, 932, 962,  //
+    962, 992, 992, 1023,                     //
+    // U plane:
+    0, 63, 127, 191, 255, 319, 383, 447,     //
+    511, 575, 639, 703, 767, 831, 895, 959,  //
+    1023,                                    //
+    // V plane:
+    0, 63, 127, 191, 255, 319, 383, 447,     //
+    511, 575, 639, 703, 767, 831, 895, 959,  //
+    1023,                                    //
+  };
+  aom_image_t img;
+  EXPECT_EQ(&img, aom_img_wrap(&img, AOM_IMG_FMT_I42016, kWidth, kHeight, 1,
+                               reinterpret_cast<uint8_t *>(buf)));
+  aom_codec_iface_t *iface = aom_codec_av1_cx();
+  aom_codec_enc_cfg_t cfg;
+  EXPECT_EQ(AOM_CODEC_OK,
+            aom_codec_enc_config_default(iface, &cfg, AOM_USAGE_GOOD_QUALITY));
+  cfg.rc_end_usage = AOM_Q;
+  cfg.g_profile = 0;
+  cfg.g_bit_depth = AOM_BITS_10;
+  cfg.g_input_bit_depth = 10;
+  cfg.g_w = kWidth;
+  cfg.g_h = kHeight;
+  cfg.g_limit = 1;
+  cfg.g_lag_in_frames = 0;
+  cfg.kf_mode = AOM_KF_DISABLED;
+  cfg.kf_max_dist = 0;
+  cfg.rc_min_quantizer = -48;
+  cfg.rc_max_quantizer = 255;
+  aom_codec_ctx_t enc;
+  EXPECT_EQ(aom_codec_enc_init(&enc, iface, &cfg, AOM_CODEC_USE_PSNR),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AOME_SET_QP, 101), AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AV1E_SET_COLOR_RANGE, AOM_CR_FULL_RANGE),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_set_option(&enc, "enable-ext-partitions", "0"),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_set_option(&enc, "enable-uneven-4way-partitions", "0"),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AOME_SET_TUNING, AOM_TUNE_SSIM),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_encode(&enc, &img, 0, 1, 0), AOM_CODEC_OK);
+  const aom_codec_cx_pkt_t *pkt;
+  aom_codec_iter_t iter = nullptr;
+  while ((pkt = aom_codec_get_cx_data(&enc, &iter)) != nullptr) {
+    if (pkt->kind == AOM_CODEC_PSNR_PKT) {
+      // In commit 2f829d09, PSNR is 39.31. But in the next commit (d7372c17),
+      // PSNR drops to 20.88.
+      EXPECT_GE(pkt->data.psnr.psnr[0], 39.0);
+    }
+  }
+  EXPECT_EQ(aom_codec_encode(&enc, nullptr, 0, 1, 0), AOM_CODEC_OK);
+  iter = nullptr;
+  EXPECT_EQ(aom_codec_get_cx_data(&enc, &iter), nullptr);
+  EXPECT_EQ(aom_codec_destroy(&enc), AOM_CODEC_OK);
+}
+
+// A test that reproduces https://gitlab.com/AOMediaCodec/avm/-/issues/717.
+TEST(EncodeAPI, DISABLED_AvmIssue717Width4) {
+  constexpr unsigned int kWidth = 4;
+  constexpr unsigned int kHeight = 34;
+  constexpr unsigned int kChromaWidth = kWidth / 2;
+  constexpr unsigned int kChromaHeight = kHeight / 2;
+  uint16_t buf[kWidth * kHeight + 2 * kChromaWidth * kChromaHeight] = {
+    // Y plane:
+    0, 28, 56, 85, 28, 56, 85, 113,           //
+    56, 85, 113, 142, 85, 113, 142, 170,      //
+    113, 142, 170, 198, 142, 170, 198, 227,   //
+    170, 198, 227, 255, 198, 227, 255, 284,   //
+    227, 255, 284, 312, 255, 284, 312, 341,   //
+    284, 312, 341, 369, 312, 341, 369, 397,   //
+    341, 369, 397, 426, 369, 397, 426, 454,   //
+    397, 426, 454, 483, 426, 454, 483, 511,   //
+    454, 483, 511, 539, 483, 511, 539, 568,   //
+    511, 539, 568, 596, 539, 568, 596, 625,   //
+    568, 596, 625, 653, 596, 625, 653, 682,   //
+    625, 653, 682, 710, 653, 682, 710, 738,   //
+    682, 710, 738, 767, 710, 738, 767, 795,   //
+    738, 767, 795, 824, 767, 795, 824, 852,   //
+    795, 824, 852, 880, 824, 852, 880, 909,   //
+    852, 880, 909, 937, 880, 909, 937, 966,   //
+    909, 937, 966, 994, 937, 966, 994, 1023,  //
+    // U plane:
+    0, 60, 60, 120, 120, 180, 180, 240,      //
+    240, 300, 300, 361, 361, 421, 421, 481,  //
+    481, 541, 541, 601, 601, 661, 661, 722,  //
+    722, 782, 782, 842, 842, 902, 902, 962,  //
+    962, 1023,                               //
+    // V plane:
+    0, 60, 60, 120, 120, 180, 180, 240,      //
+    240, 300, 300, 361, 361, 421, 421, 481,  //
+    481, 541, 541, 601, 601, 661, 661, 722,  //
+    722, 782, 782, 842, 842, 902, 902, 962,  //
+    962, 1023,                               //
+  };
+  aom_image_t img;
+  EXPECT_EQ(&img, aom_img_wrap(&img, AOM_IMG_FMT_I42016, kWidth, kHeight, 1,
+                               reinterpret_cast<uint8_t *>(buf)));
+  aom_codec_iface_t *iface = aom_codec_av1_cx();
+  aom_codec_enc_cfg_t cfg;
+  EXPECT_EQ(AOM_CODEC_OK,
+            aom_codec_enc_config_default(iface, &cfg, AOM_USAGE_GOOD_QUALITY));
+  cfg.rc_end_usage = AOM_Q;
+  cfg.g_profile = 0;
+  cfg.g_bit_depth = AOM_BITS_10;
+  cfg.g_input_bit_depth = 10;
+  cfg.g_w = kWidth;
+  cfg.g_h = kHeight;
+  cfg.g_limit = 1;
+  cfg.g_lag_in_frames = 0;
+  cfg.kf_mode = AOM_KF_DISABLED;
+  cfg.kf_max_dist = 0;
+  cfg.rc_min_quantizer = -48;
+  cfg.rc_max_quantizer = 255;
+  aom_codec_ctx_t enc;
+  EXPECT_EQ(aom_codec_enc_init(&enc, iface, &cfg, AOM_CODEC_USE_PSNR),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AOME_SET_QP, 101), AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AV1E_SET_COLOR_RANGE, AOM_CR_FULL_RANGE),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_set_option(&enc, "enable-ext-partitions", "0"),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_set_option(&enc, "enable-uneven-4way-partitions", "0"),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_control(&enc, AOME_SET_TUNING, AOM_TUNE_SSIM),
+            AOM_CODEC_OK);
+  EXPECT_EQ(aom_codec_encode(&enc, &img, 0, 1, 0), AOM_CODEC_OK);
+  const aom_codec_cx_pkt_t *pkt;
+  aom_codec_iter_t iter = nullptr;
+  while ((pkt = aom_codec_get_cx_data(&enc, &iter)) != nullptr) {
+    if (pkt->kind == AOM_CODEC_PSNR_PKT) {
+      // In commit 2f829d09, PSNR is 39.26. But in the next commit (d7372c17),
+      // PSNR drops to 28.96.
+      EXPECT_GE(pkt->data.psnr.psnr[0], 39.0);
+    }
+  }
+  EXPECT_EQ(aom_codec_encode(&enc, nullptr, 0, 1, 0), AOM_CODEC_OK);
+  iter = nullptr;
+  EXPECT_EQ(aom_codec_get_cx_data(&enc, &iter), nullptr);
+  EXPECT_EQ(aom_codec_destroy(&enc), AOM_CODEC_OK);
+}
+
 // A test that reproduces https://gitlab.com/AOMediaCodec/avm/-/issues/746.
 TEST(EncodeAPI, DISABLED_GdfOptimizer8x8) {
   constexpr unsigned int kWidth = 8;
