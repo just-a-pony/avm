3356493b81c56d9b46da43466a78975699447dfd
Urvang Joshi <urvang@google.com>
2025-08-20 22:39:44 +0000

    Disable CONFIG_DF_DQP for now
    STATS_CHANGED
    
    This is a temporary workaround for #836

--- av1/common/av1_loopfilter.c
+++ av1/common/av1_loopfilter.c
@@ -298,12 +298,12 @@ void av1_loop_filter_frame_init(AV1_COMMON *cm, int plane_start,
         const int seg_lf_feature_id = seg_lvl_lf_lut[plane][dir];
 #endif  // CONFIG_DF_DQP
         if (segfeature_active(seg, seg_id, seg_lf_feature_id)) {
-          const int data = get_segdata(&cm->seg, seg_id, seg_lf_feature_id);
           // TODO(Andrey): add separate offsets to segments for q and side
           // thresholds // add clamp
 #if !CONFIG_DF_DQP
           break;
 #endif  // !CONFIG_DF_DQP
+          const int data = get_segdata(&cm->seg, seg_id, seg_lf_feature_id);
           q_ind_seg += data;
           side_ind_seg += data;
         }
