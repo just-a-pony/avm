just-a-pony/avm

commit 58e04a24afef3413873924e49bf54ec998001426

CWG-F243: Remove enable order hint flag. STATS_CHANGED.
This MR will remove the enable order hint flag.
STATS CHANGED

av1/encoder/bitstream.c
@@ -5699,9 +5699,11 @@ static AOM_INLINE void write_sequence_header(
     aom_wb_write_bit(wb, seq_params->enable_six_param_warp_delta);
 
     aom_wb_write_bit(wb, seq_params->enable_masked_compound);
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     aom_wb_write_bit(wb, seq_params->order_hint_info.enable_order_hint);
 
     if (seq_params->order_hint_info.enable_order_hint) {
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
       aom_wb_write_bit(wb, seq_params->order_hint_info.enable_ref_frame_mvs);
 #if CONFIG_REDUCED_REF_FRAME_MVS_MODE
       if (seq_params->order_hint_info.enable_ref_frame_mvs) {
@@ -5711,7 +5713,9 @@ static AOM_INLINE void write_sequence_header(
             wb, seq_params->order_hint_info.reduced_ref_frame_mvs_mode);
       }
 #endif  // CONFIG_REDUCED_REF_FRAME_MVS_MODE
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     }
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     if (seq_params->force_screen_content_tools == 2) {
       aom_wb_write_bit(wb, 1);
     } else {
@@ -5728,7 +5732,9 @@ static AOM_INLINE void write_sequence_header(
     } else {
       assert(seq_params->force_integer_mv == 2);
     }
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     if (seq_params->order_hint_info.enable_order_hint)
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
       aom_wb_write_literal(
           wb, seq_params->order_hint_info.order_hint_bits_minus_1, 3);
   }
@@ -5942,9 +5948,13 @@ static AOM_INLINE void write_sequence_header_beyond_av1(
     aom_wb_write_bit(wb, seq_params->enable_tip_explicit_qp);
   }
   aom_wb_write_bit(wb, seq_params->enable_orip);
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   if (seq_params->order_hint_info.enable_order_hint) {
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     aom_wb_write_literal(wb, seq_params->enable_opfl_refine, 2);
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   }
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   aom_wb_write_bit(wb, seq_params->enable_ibp);
   aom_wb_write_bit(wb, seq_params->enable_adaptive_mvd);
   aom_wb_write_bit(wb, seq_params->enable_refinemv);
@@ -6136,20 +6146,24 @@ static AOM_INLINE void write_global_motion(AV1_COMP *cpi,
   for (frame = 0; frame < num_total_refs; ++frame) {
 #if CONFIG_IMPROVED_GLOBAL_MOTION
     int temporal_distance;
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     if (seq_params->order_hint_info.enable_order_hint) {
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
       const RefCntBuffer *const ref_buf = get_ref_frame_buf(cm, frame);
 #if CONFIG_EXPLICIT_TEMPORAL_DIST_CALC
       const int ref_order_hint = ref_buf->display_order_hint;
       const int cur_order_hint = cm->cur_frame->display_order_hint;
 #else
-      const int ref_order_hint = ref_buf->order_hint;
-      const int cur_order_hint = cm->cur_frame->order_hint;
+    const int ref_order_hint = ref_buf->order_hint;
+    const int cur_order_hint = cm->cur_frame->order_hint;
 #endif  // CONFIG_EXPLICIT_TEMPORAL_DIST_CALC
       temporal_distance = get_relative_dist(&seq_params->order_hint_info,
                                             cur_order_hint, ref_order_hint);
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     } else {
       temporal_distance = 1;
     }
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 
     if (temporal_distance == 0) {
       // Don't code global motion for frames at the same temporal instant
@@ -6316,7 +6330,9 @@ static AOM_INLINE void write_uncompressed_header_obu(
                                cm->height != seq_params->max_frame_height);
     if (!frame_is_sframe(cm)) aom_wb_write_bit(wb, frame_size_override_flag);
 
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     if (seq_params->order_hint_info.enable_order_hint)
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
       aom_wb_write_literal(
           wb, current_frame->order_hint,
           seq_params->order_hint_info.order_hint_bits_minus_1 + 1);
@@ -6418,8 +6434,11 @@ static AOM_INLINE void write_uncompressed_header_obu(
       current_frame->refresh_frame_flags != REFRESH_FRAME_ALL) {
 #endif  // CONFIG_EXTRA_DPB
     // Write all ref frame order hints if error_resilient_mode == 1
-    if (features->error_resilient_mode &&
-        seq_params->order_hint_info.enable_order_hint) {
+    if (features->error_resilient_mode
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+        && seq_params->order_hint_info.enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+    ) {
       for (int ref_idx = 0; ref_idx < cm->seq_params.ref_frames; ref_idx++) {
         aom_wb_write_literal(
             wb, cm->ref_frame_map[ref_idx]->order_hint,
@@ -6513,8 +6532,11 @@ static AOM_INLINE void write_uncompressed_header_obu(
       // error resilient mode
       const int explicit_ref_frame_map =
           cm->features.error_resilient_mode || frame_is_sframe(cm) ||
-          seq_params->explicit_ref_frame_map ||
-          !seq_params->order_hint_info.enable_order_hint;
+          seq_params->explicit_ref_frame_map
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+          || !seq_params->order_hint_info.enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+          ;
       if (explicit_ref_frame_map) {
 #if CONFIG_ACROSS_SCALE_REF_OPT
         if (cm->ref_frames_info.num_total_refs < 0 ||
@@ -6579,8 +6601,11 @@ static AOM_INLINE void write_uncompressed_header_obu(
       }
 
       if (features->allow_ref_frame_mvs &&
-          cm->ref_frames_info.num_total_refs > 1 &&
-          seq_params->order_hint_info.enable_order_hint) {
+          cm->ref_frames_info.num_total_refs > 1
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+          && seq_params->order_hint_info.enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+      ) {
         // Write TMVP sampling mode
         assert(cm->tmvp_sample_step == 1 || cm->tmvp_sample_step == 2);
         aom_wb_write_bit(wb, cm->tmvp_sample_step - 1);
@@ -7895,12 +7920,17 @@ int av1_pack_bitstream(AV1_COMP *const cpi, uint8_t *dst, size_t *size,
 
   const int write_frame_header =
       (cpi->num_tg > 1 ||
-       (encode_show_existing_frame(cm) &&
-        (!cm->seq_params.order_hint_info.enable_order_hint
+       (encode_show_existing_frame(cm)
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+        && (!cm->seq_params.order_hint_info.enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 #if !CONFIG_F253_REMOVE_OUTPUTFLAG
-         || !cm->seq_params.enable_frame_output_order
+            || !cm->seq_params.enable_frame_output_order
 #endif  // !CONFIG_F253_REMOVE_OUTPUTFLAG
-         )) ||
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+            )
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+            ) ||
        (encode_show_existing_frame(cm) &&
         cm->cur_frame->frame_type == KEY_FRAME) ||
 #if CONFIG_BRU
@@ -7940,28 +7970,39 @@ int av1_pack_bitstream(AV1_COMP *const cpi, uint8_t *dst, size_t *size,
   }
 #endif  // CONFIG_BRU
 
-#if CONFIG_F253_REMOVE_OUTPUTFLAG
-  // The OBU packet of show_existing_frame
+#if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
+  const bool non_signaled_show_existing_frame =
+      (cm->show_existing_frame && !cm->features.error_resilient_mode) ||
+      encode_show_existing_frame(cm);
+#elif !CONFIG_F253_REMOVE_OUTPUTFLAG
+  const bool non_signaled_show_existing_frame =
+      (cm->seq_params.enable_frame_output_order && cm->show_existing_frame &&
+       !cm->features.error_resilient_mode) ||
+      (!cm->seq_params.enable_frame_output_order &&
+       encode_show_existing_frame(cm));
+#elif !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+    const bool non_signaled_show_existing_frame =
+        (cm->seq_params.order_hint_info.enable_order_hint &&
+         cm->show_existing_frame && !cm->features.error_resilient_mode) ||
+        (!cm->seq_params.order_hint_info.enable_order_hint &&
+         encode_show_existing_frame(cm));
 #else
-  // When enable_frame_output_order == 1, the OBU packet of show_existing_frame
-#endif  // CONFIG_F253_REMOVE_OUTPUTFLAG
-  // is not signaled for non-error-resilient mode.
-  // For error-resilienet mode, still an OBU is signaled.
-  if ((cm->seq_params.order_hint_info.enable_order_hint &&
-#if !CONFIG_F253_REMOVE_OUTPUTFLAG
-       cm->seq_params.enable_frame_output_order &&
-#endif  // !CONFIG_F253_REMOVE_OUTPUTFLAG
-       cm->show_existing_frame && !cm->features.error_resilient_mode) ||
-      ((!cm->seq_params.order_hint_info.enable_order_hint
-#if !CONFIG_F253_REMOVE_OUTPUTFLAG
-        || !cm->seq_params.enable_frame_output_order
-#endif  // !CONFIG_F253_REMOVE_OUTPUTFLAG
-        ) &&
-       encode_show_existing_frame(cm)) ||
+    const bool non_signaled_show_existing_frame =
+        (cm->seq_params.order_hint_info.enable_order_hint &&
+         cm->seq_params.enable_frame_output_order && cm->show_existing_frame &&
+         !cm->features.error_resilient_mode) ||
+        ((!cm->seq_params.order_hint_info.enable_order_hint ||
+          !cm->seq_params.enable_frame_output_order) &&
+         encode_show_existing_frame(cm));
+#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT &&
+        // CONFIG_F253_REMOVE_OUTPUTFLAG
+  const bool non_signaled_frame =
+      non_signaled_show_existing_frame ||
 #if CONFIG_BRU
       cm->bru.frame_inactive_flag ||
 #endif  // CONFIG_BRU
-      (cm->features.tip_frame_mode == TIP_FRAME_AS_OUTPUT)) {
+      (cm->features.tip_frame_mode == TIP_FRAME_AS_OUTPUT);
+  if (non_signaled_frame) {
     data_size = 0;
   } else {
     // Since length_field is determined adaptively after frame header

aom/aomcx.h
@@ -800,6 +800,7 @@ enum aome_enc_control_id {
    */
   AV1E_SET_ENABLE_INTRA_EDGE_FILTER = 78,
 
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   /*!\brief Codec control function to turn on / off frame order hint (int
    * parameter). Affects: joint compound mode, motion field motion vector,
    * ref frame sign bias
@@ -808,6 +809,7 @@ enum aome_enc_control_id {
    * - 1 = enable (default)
    */
   AV1E_SET_ENABLE_ORDER_HINT = 79,
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 
   /*!\brief Codec control function to turn on / off 64-length transforms, int
    * parameter
@@ -1584,8 +1586,10 @@ AOM_CTRL_USE_TYPE(AV1E_SET_MAX_PARTITION_SIZE, int)
 AOM_CTRL_USE_TYPE(AV1E_SET_ENABLE_INTRA_EDGE_FILTER, int)
 #define AOM_CTRL_AV1E_SET_ENABLE_INTRA_EDGE_FILTER
 
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 AOM_CTRL_USE_TYPE(AV1E_SET_ENABLE_ORDER_HINT, int)
 #define AOM_CTRL_AV1E_SET_ENABLE_ORDER_HINT
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 
 AOM_CTRL_USE_TYPE(AV1E_SET_ENABLE_TX64, int)
 #define AOM_CTRL_AV1E_SET_ENABLE_TX64

apps/aomenc.c
@@ -149,7 +149,9 @@ static const int av1_arg_ctrl_map[] = { AOME_SET_CPUUSED,
                                         AV1E_SET_MAX_PARTITION_SIZE,
                                         AV1E_SET_ENABLE_CHROMA_DELTAQ,
                                         AV1E_SET_ENABLE_INTRA_EDGE_FILTER,
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
                                         AV1E_SET_ENABLE_ORDER_HINT,
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
                                         AV1E_SET_ENABLE_TX64,
                                         AV1E_SET_ENABLE_FLIP_IDTX,
                                         AV1E_SET_ENABLE_MASKED_COMP,
@@ -347,7 +349,9 @@ const arg_def_t *av1_ctrl_args[] = {
   &g_av1_codec_arg_defs.max_partition_size,
   &g_av1_codec_arg_defs.enable_chroma_deltaq,
   &g_av1_codec_arg_defs.enable_intra_edge_filter,
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   &g_av1_codec_arg_defs.enable_order_hint,
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   &g_av1_codec_arg_defs.enable_tx64,
   &g_av1_codec_arg_defs.enable_flip_idtx,
   &g_av1_codec_arg_defs.enable_masked_comp,

av1/arg_defs.c
@@ -514,9 +514,11 @@ const av1_codec_arg_definitions_t g_av1_codec_arg_defs = {
   .enable_intra_edge_filter = ARG_DEF(NULL, "enable-intra-edge-filter", 1,
                                       "Enable intra edge filtering "
                                       "(0: false, 1: true (default))"),
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   .enable_order_hint = ARG_DEF(NULL, "enable-order-hint", 1,
                                "Enable order hint "
                                "(0: false, 1: true (default))"),
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   .enable_tx64 =
       ARG_DEF(NULL, "enable-tx64", 1,
               "Enable 64-pt transform (0: false, 1: true (default))"),

av1/arg_defs.h
@@ -190,7 +190,9 @@ typedef struct av1_codec_arg_definitions {
   arg_def_t max_partition_size;
   arg_def_t enable_chroma_deltaq;
   arg_def_t enable_intra_edge_filter;
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   arg_def_t enable_order_hint;
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   arg_def_t enable_tx64;
 #if CONFIG_REDUCED_TX_PART
   arg_def_t reduced_tx_part_set;

av1/av1_cx_iface.c
@@ -171,8 +171,10 @@ struct av1_extracfg {
   int min_partition_size;        // min partition size [4,8,16,32,64,128]
   int max_partition_size;        // max partition size [4,8,16,32,64,128]
   int enable_intra_edge_filter;  // enable intra-edge filter for sequence
-  int enable_order_hint;         // enable order hint for sequence
-  int enable_tx64;               // enable 64-pt transform usage for sequence
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+  int enable_order_hint;  // enable order hint for sequence
+#endif                    // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+  int enable_tx64;        // enable 64-pt transform usage for sequence
 #if CONFIG_REDUCED_TX_PART
   int reduced_tx_part_set;   // enable reduced transform block partition set
 #endif                       // CONFIG_REDUCED_TX_PART
@@ -539,7 +541,9 @@ static struct av1_extracfg default_extra_cfg = {
   4,    // min_partition_size
   256,  // max_partition_size
   1,    // enable intra edge filter
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   1,    // frame order hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   1,    // enable 64-pt transform usage
 #if CONFIG_REDUCED_TX_PART
   0,    // enable reduced transform block partition set
@@ -1562,9 +1566,14 @@ static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
   tool_cfg->enable_monochrome = cfg->monochrome;
   tool_cfg->full_still_picture_hdr = cfg->full_still_picture_hdr;
   tool_cfg->enable_tcq = cfg->enable_tcq;
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   tool_cfg->enable_order_hint = extra_cfg->enable_order_hint;
-  tool_cfg->ref_frame_mvs_present =
-      extra_cfg->enable_ref_frame_mvs & extra_cfg->enable_order_hint;
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+  tool_cfg->ref_frame_mvs_present = extra_cfg->enable_ref_frame_mvs
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+                                    & extra_cfg->enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+      ;
   tool_cfg->enable_global_motion = extra_cfg->enable_global_motion;
   tool_cfg->enable_skip_mode = extra_cfg->enable_skip_mode;
   tool_cfg->error_resilient_mode =
@@ -1598,7 +1607,11 @@ static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
   } else {
     tool_cfg->avg_cdf_type = 0;
   }
-  if (extra_cfg->enable_order_hint && extra_cfg->enable_ref_frame_mvs) {
+  if (
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+      extra_cfg->enable_order_hint &&
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+      extra_cfg->enable_ref_frame_mvs) {
     tool_cfg->enable_tip = extra_cfg->enable_tip;
     tool_cfg->enable_mv_traj = extra_cfg->enable_mv_traj;
     if (tool_cfg->enable_tip) {
@@ -1622,7 +1635,9 @@ static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
 #endif  // CONFIG_MV_RANGE_EXTENSION
 
 #if CONFIG_FRAME_HEADER_SIGNAL_OPT
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   if (extra_cfg->enable_order_hint) {
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
     tool_cfg->enable_opfl_refine = extra_cfg->enable_opfl_refine;
     if (tool_cfg->enable_opfl_refine) {
       if (cfg->g_lag_in_frames == 0) {
@@ -1633,13 +1648,19 @@ static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
         tool_cfg->enable_opfl_refine = 0;
       }
     }
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   } else {
     tool_cfg->enable_opfl_refine = AOM_OPFL_REFINE_NONE;
   }
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 #else
-  tool_cfg->enable_opfl_refine = extra_cfg->enable_order_hint
-                                     ? extra_cfg->enable_opfl_refine
-                                     : AOM_OPFL_REFINE_NONE;
+  tool_cfg->enable_opfl_refine =
+#if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+      extra_cfg->enable_opfl_refine;
+#else
+      extra_cfg->enable_order_hint ? extra_cfg->enable_opfl_refine
+                                   : AOM_OPFL_REFINE_NONE;
+#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 #endif  // CONFIG_FRAME_HEADER_SIGNAL_OPT
 #if CONFIG_ENABLE_TIP_REFINEMV_SEQ_FLAG
   tool_cfg->enable_tip_refinemv =
@@ -1847,8 +1868,11 @@ static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
   // when order_hint is not available, S-frame is used or error resilience mode
   // is used.
   oxcf->ref_frm_cfg.enable_frame_output_order =
-      (!tool_cfg->enable_order_hint || kf_cfg->enable_sframe ||
-       tool_cfg->error_resilient_mode)
+      (
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+          !tool_cfg->enable_order_hint ||
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+          kf_cfg->enable_sframe || tool_cfg->error_resilient_mode)
           ? 0
 #if CONFIG_F253_REMOVE_OUTPUTFLAG
           : 1;
@@ -2493,12 +2517,14 @@ static aom_codec_err_t ctrl_set_enable_intra_edge_filter(
   return update_extra_cfg(ctx, &extra_cfg);
 }
 
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 static aom_codec_err_t ctrl_set_enable_order_hint(aom_codec_alg_priv_t *ctx,
                                                   va_list args) {
   struct av1_extracfg extra_cfg = ctx->extra_cfg;
   extra_cfg.enable_order_hint = CAST(AV1E_SET_ENABLE_ORDER_HINT, args);
   return update_extra_cfg(ctx, &extra_cfg);
 }
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 
 static aom_codec_err_t ctrl_set_enable_tx64(aom_codec_alg_priv_t *ctx,
                                             va_list args) {
@@ -4316,9 +4342,11 @@ static aom_codec_err_t encoder_set_option(aom_codec_alg_priv_t *ctx,
                               argv, err_string)) {
     extra_cfg.enable_intra_edge_filter =
         arg_parse_uint_helper(&arg, err_string);
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   } else if (arg_match_helper(&arg, &g_av1_codec_arg_defs.enable_order_hint,
                               argv, err_string)) {
     extra_cfg.enable_order_hint = arg_parse_int_helper(&arg, err_string);
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   } else if (arg_match_helper(&arg, &g_av1_codec_arg_defs.enable_tx64, argv,
                               err_string)) {
     extra_cfg.enable_tx64 = arg_parse_int_helper(&arg, err_string);
@@ -4666,7 +4694,9 @@ static aom_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
   { AV1E_SET_MAX_PARTITION_SIZE, ctrl_set_max_partition_size },
   { AV1E_SET_ENABLE_CHROMA_DELTAQ, ctrl_set_enable_chroma_deltaq },
   { AV1E_SET_ENABLE_INTRA_EDGE_FILTER, ctrl_set_enable_intra_edge_filter },
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   { AV1E_SET_ENABLE_ORDER_HINT, ctrl_set_enable_order_hint },
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   { AV1E_SET_ENABLE_TX64, ctrl_set_enable_tx64 },
   { AV1E_SET_ENABLE_FLIP_IDTX, ctrl_set_enable_flip_idtx },
   { AV1E_SET_MAX_REFERENCE_FRAMES, ctrl_set_max_reference_frames },

av1/av1_dx_iface.c
@@ -817,8 +817,10 @@ static aom_codec_err_t decoder_decode(aom_codec_alg_priv_t *ctx,
     // that have showable_frame == 1 but have not yet been output.  This is
     // useful when OBUs are lost due to channel errors or removed for temporal
     // scalability.
-    if (data == NULL && data_sz == 0 &&
-        pbi->common.seq_params.order_hint_info.enable_order_hint
+    if (data == NULL && data_sz == 0
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+        && pbi->common.seq_params.order_hint_info.enable_order_hint
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 #if !CONFIG_F253_REMOVE_OUTPUTFLAG
         && pbi->common.seq_params.enable_frame_output_order
 #endif  // !CONFIG_F253_REMOVE_OUTPUTFLAG

av1/common/av1_common_int.h
@@ -530,7 +530,9 @@ typedef struct {
 } DeltaQInfo;
 
 typedef struct {
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   int enable_order_hint;        // 0 - disable order hint, and related tools
+#endif                          // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
   int order_hint_bits_minus_1;  // dist_wtd_comp, ref_frame_mvs,
                                 // frame_sign_bias
                                 // if 0, enable_dist_wtd_comp and
@@ -2436,7 +2438,9 @@ static INLINE RefCntBuffer *get_primary_ref_frame_buf(
 static INLINE int frame_might_allow_ref_frame_mvs(const AV1_COMMON *cm) {
   return !cm->features.error_resilient_mode &&
          cm->seq_params.order_hint_info.enable_ref_frame_mvs &&
+#if !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
          cm->seq_params.order_hint_info.enable_order_hint &&
+#endif  // !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
          !frame_is_intra_only(cm);
 }
 
@@ -4703,7 +4707,11 @@ static INLINE void init_ibp_info(
 #define RELATIVE_DIST_BITS 8
 
 static INLINE int get_relative_dist(const OrderHintInfo *oh, int a, int b) {
+#if CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+  if (oh->order_hint_bits_minus_1 < 0) return 0;
+#else
   if (!oh->enable_order_hint) return 0;
+#endif  // CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
 
 #if CONFIG_EXPLICIT_TEMPORAL_DIST_CALC
   assert(a >= 0);

commit c3e70cdf04772639131b0dbeea7d686c2db73396

Fix two `#elif` conditions
Otherwise the `#else` branch is unreachable.

This bug was introduced in
https://gitlab.com/AOMediaCodec/avm/-/commit/58e04a24.

av1/encoder/bitstream.c
@@ -7944,13 +7944,13 @@ int av1_pack_bitstream(AV1_COMP *const cpi, uint8_t *dst, size_t *size,
   const bool non_signaled_show_existing_frame =
       (cm->show_existing_frame && !cm->features.error_resilient_mode) ||
       encode_show_existing_frame(cm);
-#elif !CONFIG_F253_REMOVE_OUTPUTFLAG
+#elif CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && !CONFIG_F253_REMOVE_OUTPUTFLAG
   const bool non_signaled_show_existing_frame =
       (cm->seq_params.enable_frame_output_order && cm->show_existing_frame &&
        !cm->features.error_resilient_mode) ||
       (!cm->seq_params.enable_frame_output_order &&
        encode_show_existing_frame(cm));
-#elif !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT
+#elif !CONFIG_CWG_F243_REMOVE_ENABLE_ORDER_HINT && CONFIG_F253_REMOVE_OUTPUTFLAG
                   const bool non_signaled_show_existing_frame =
                       (cm->seq_params.order_hint_info.enable_order_hint &&
                        cm->show_existing_frame &&