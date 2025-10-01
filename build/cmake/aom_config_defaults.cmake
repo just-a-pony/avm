ccafb97c934f96bd33da9455cbe261ca3b2939f7
Lester Lu <kslu@google.com>
2025-10-01 21:47:15 +0000

    Clean up a removed flag
    Remove CONFIG_DAMR_CLEAN_UP
    
    The flag was removed before v11 anchor but definition still exists.
    
    #912

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -353,9 +353,6 @@ set_aom_config_var(CONFIG_GDF_IMPROVEMENT 1
 set_aom_config_var(CONFIG_REDUCE_SYMBOL_SIZE 1
                    "Symbol size reduction from 16 to 8.")
 
-set_aom_config_var(CONFIG_DAMR_CLEAN_UP 1
-                   "Clean up DAMR memory bandwith issue.")
-
 set_aom_config_var(CONFIG_TMVP_MVS_WRITING_FLOW_OPT 1
                    "Enable to only write TMVP MVs once")
 
